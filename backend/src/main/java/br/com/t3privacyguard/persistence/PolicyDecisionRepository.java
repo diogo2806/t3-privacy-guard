6a630c1e0bb562ba8c629e1b5aabef314a3a2bec
Diogo Santos <diogo_2806@hotmail.com>
2026-09-12 20:54:11 -0300

    feat(privacy): support policy decision purge (#40)

--- backend/src/main/java/br/com/t3privacyguard/persistence/PolicyDecisionRepository.java
+++ backend/src/main/java/br/com/t3privacyguard/persistence/PolicyDecisionRepository.java
@@ -1,8 +1,10 @@
 package br.com.t3privacyguard.persistence;
 
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PolicyDecisionRepository extends JpaRepository<PolicyDecisionEntity, String> {
     Optional<PolicyDecisionEntity> findByActionProposalId(String actionProposalId);
+    void deleteAllByActionProposalIdIn(List<String> actionProposalIds);
 }
