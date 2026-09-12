59d2f4860e373c511fb63a0f9466ba5f7cf20434
Diogo Santos <diogo_2806@hotmail.com>
2026-09-12 20:54:18 -0300

    feat(privacy): support remediation purge (#40)

--- backend/src/main/java/br/com/t3privacyguard/persistence/RemediationExecutionRepository.java
+++ backend/src/main/java/br/com/t3privacyguard/persistence/RemediationExecutionRepository.java
@@ -1,8 +1,10 @@
 package br.com.t3privacyguard.persistence;
 
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RemediationExecutionRepository extends JpaRepository<RemediationExecutionEntity,String>{
     Optional<RemediationExecutionEntity> findByActionProposalId(String actionProposalId);
+    void deleteAllByActionProposalIdIn(List<String> actionProposalIds);
 }
