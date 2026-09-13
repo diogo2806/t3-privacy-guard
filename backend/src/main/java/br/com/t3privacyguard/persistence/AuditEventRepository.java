e12b5b52e8ab9bf595027ae16c02ed962cfdbd55
Diogo Santos <diogo_2806@hotmail.com>
2026-09-12 22:59:35 -0300

    feat: suportar verificação da cadeia de audit (#57)

--- backend/src/main/java/br/com/t3privacyguard/persistence/AuditEventRepository.java
+++ backend/src/main/java/br/com/t3privacyguard/persistence/AuditEventRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AuditEventRepository extends JpaRepository<AuditEventEntity, String> {
     List<AuditEventEntity> findByIncidentIdOrderByCreatedAtAsc(String incidentId);
+    boolean existsByIncidentIdAndIntegritySequenceIsNotNull(String incidentId);
     void deleteAllByIncidentId(String incidentId);
 }
