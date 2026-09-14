28af0e9bdbb6ce0798506632012a599de71e7dd1
Diogo Santos <diogo_2806@hotmail.com>
2026-09-14 10:01:48 -0300

    feat(metrics): reject unsupported impact windows explicitly

---
+++ backend/src/main/java/br/com/t3privacyguard/service/InvalidBusinessImpactWindowException.java
@@ -0,0 +1,7 @@
+package br.com.t3privacyguard.service;
+
+public class InvalidBusinessImpactWindowException extends RuntimeException {
+    public InvalidBusinessImpactWindowException(String message) {
+        super(message);
+    }
+}
