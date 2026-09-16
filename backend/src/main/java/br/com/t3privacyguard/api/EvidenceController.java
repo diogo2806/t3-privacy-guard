0f0990f9c8304aa79ef7701a2ff9ef5ecb6fa56a
Diogo Santos <diogo_2806@hotmail.com>
2026-09-16 12:33:55 -0300

    fix: return no-content when live evidence is not generated (#214)

--- backend/src/main/java/br/com/t3privacyguard/api/EvidenceController.java
+++ backend/src/main/java/br/com/t3privacyguard/api/EvidenceController.java
@@ -1,7 +1,9 @@
 package br.com.t3privacyguard.api;
 
+import br.com.t3privacyguard.service.EvidenceNotFoundException;
 import br.com.t3privacyguard.service.EvidenceService;
 import br.com.t3privacyguard.service.EvidenceService.EvidenceResponse;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +18,11 @@ public class EvidenceController {
     }
 
     @GetMapping("/latest")
-    public EvidenceResponse latest() {
-        return service.latest();
+    public ResponseEntity<EvidenceResponse> latest() {
+        try {
+            return ResponseEntity.ok(service.latest());
+        } catch (EvidenceNotFoundException ex) {
+            return ResponseEntity.noContent().build();
+        }
     }
 }
